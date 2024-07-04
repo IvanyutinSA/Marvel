ef4360528abd13e10e2f97287b0b337e0cc723ed
Sergey <ivanyutin2004@gmail.com>
2024-07-04 22:38:00 +0600

    Basic application complete

--- app/src/main/java/com/example/marvel/MainActivity.kt
+++ app/src/main/java/com/example/marvel/MainActivity.kt
@@ -39,6 +39,8 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
@@ -58,8 +60,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.paint
+import androidx.compose.ui.graphics.BlendMode
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.HorizontalAlignmentLine
@@ -81,6 +86,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
+import coil.ImageLoader
 import coil.compose.AsyncImagePainter
 import coil.compose.rememberAsyncImagePainter
 import coil.request.ImageRequest
@@ -94,6 +100,7 @@ data class Character(
     val painter: Painter,
     val backgroundColor: Color,
     val description: String = "",
+    val logoUrl: String = "",
 )
 
 class MainActivity : ComponentActivity() {
@@ -117,37 +124,32 @@ data class Env(
 
 @Composable
 fun InitCharacters(env: Env) {
-    var x = rememberAsyncImagePainter(
-        ImageRequest
-            .Builder(LocalContext.current)
-            .data("https://i.postimg.cc/W1Nkbxb2/rei.jpg")
-            .build(),
-        placeholder = painterResource(id = R.drawable.asuka),
-        error = painterResource(id = R.drawable.rei),
-    )
+    var x = rememberAsyncImagePainter(model = "https://i.postimg.cc/W1Nkbxb2/rei.jpg", imageLoader = ImageLoader(context = LocalContext.current))
     env.characters = arrayOf(
         Character(
-            name = "Langley Asuka",
-            painter = painterResource(id = R.drawable.asuka),
+            name = "Spider Man",
+            painter = rememberAsyncImagePainter("https://cdn.kinocheck.com/i/w=375/lq3jw5b1y2.jpg"),
             backgroundColor = Color.LightGray,
-            description = "is a 14-year-old fictional character from the Neon Genesis Evangelion franchise and one of the main female characters. Asuka is designated as the Second Child (\"Second Children\" in the original Japanese versions) of the Evangelion Project and pilots the Evangelion Unit-02. Her surname is romanized as Soryu in the English manga and Sohryu in the English version of the TV series, the English version of the film, and on GAINAXs website."
+            description = "Peter Parker is a superhero portrayed by Tom Holland in the Marvel Cinematic Universe (MCU) media franchise–based on the Marvel Comics character of the same name—also known by his alias, Spider-Man. Parker is initially depicted as a student at the Midtown School of Science and Technology who received spider-like and superhuman abilities after being bitten by a radioactive spider. Parker initially uses his powers to fight crime as a vigilante in Queens. ",
         ),
         Character(
-            name = "Ayanami Rei",
-            painter=  painterResource(id = R.drawable.rei),
-            backgroundColor = Color.Blue,
+            name = "Captain America",
+            painter = rememberAsyncImagePainter("https://static.wikia.nocookie.net/villains/images/9/91/CaptainUnited..png/revision/latest?cb=20230629133146"),
+            backgroundColor = Color.LightGray,
+            description = "Captain America is a superhero created by Joe Simon and Jack Kirby who appears in American comic books published by Marvel Comics. The character first appeared in Captain America Comics #1, published on December 20, 1940, by Timely Comics, a corporate predecessor to Marvel. Captain America's civilian identity is Steve Rogers, a frail man enhanced to the peak of human physical perfection by an experimental \"super-soldier serum\" after joining the United States Army to aid the country's efforts in World War II. Equipped with an American flag–inspired costume and a virtually indestructible shield, Captain America and his sidekick Bucky Barnes clashed frequently with the villainous Red Skull and other members of the Axis powers. In the final days of the war, an accident left Captain America frozen in a state of suspended animation until he was revived in modern times. He resumes his exploits as a costumed hero and becomes leader of the superhero team the Avengers, but frequently struggles as a \"man out of time\" to adjust to the new era.",
         ),
         Character(
-            name = "Misato",
-            painter=  painterResource(id = R.drawable.misato),
-            backgroundColor = Color.Red,
+            name = "Iron Man",
+            painter = rememberAsyncImagePainter("https://cdn.britannica.com/49/182849-050-4C7FE34F/scene-Iron-Man.jpg"),
+            backgroundColor = Color.LightGray,
+            description = "Iron Man is a superhero appearing in American comic books published by Marvel Comics. Co-created by writer and editor Stan Lee, developed by scripter Larry Lieber, and designed by artists Don Heck and Jack Kirby, the character first appeared in Tales of Suspense #39 in 1962 and received his own title with Iron Man #1 in 1968. Shortly after his creation, Iron Man became a founding member of the superhero team, the Avengers, with Thor, Ant-Man, the Wasp, and the Hulk. Iron Man stories, individually and with the Avengers, have been published consistently since the character's creation. ",
         ),
-//        Character(
-//            name = "Spider Man",
-//            painter = x,
-//            backgroundColor = Color.LightGray,
-//            description = "SPIDAR MAN"
-//        ),
+        Character(
+            name = "Thanos",
+            painter = rememberAsyncImagePainter("https://static.wikia.nocookie.net/marvelcinematicuniverse/images/5/52/Empire_March_Cover_IW_6_Textless.png/revision/latest?cb=20231024024627"),
+            backgroundColor = Color.LightGray,
+            description = "Thanos is a fictional character portrayed primarily by Josh Brolin in the Marvel Cinematic Universe (MCU) media franchise, based on the Marvel Comics supervillain of the same name. He is depicted as an alien warlord from the doomed planet Titan with a universe-spanning agenda to wipe out half of all life to stabilize overpopulation and prevent what he views as life's inevitable extinction. To do this, he sets out to obtain the six Infinity Stones, cosmic gems with the power to achieve his goal. With the help of his adopted children, Thanos fights against the Avengers, the Guardians of the Galaxy, and their allies, in the Infinity War, succeeds in assembling the Stones, and disintegrates half of all life in the universe in an event that is known as the Blip. After escaping to the Garden and destroying the Stones, he is eventually killed by Thor. Five years later, an alternate version of Thanos from 2014 time travels to 2023 to battle the Avengers once again, but is killed by Tony Stark. ",
+        ),
     )
 }
 
@@ -194,6 +196,7 @@ fun ScreenCharacters(
 ) {
 
     val lazyListState = rememberLazyListState()
+    var characterColor by remember { mutableStateOf(Color.Red)}
 
     val textModifier = modifier
         .fillMaxWidth();
@@ -211,7 +214,8 @@ fun ScreenCharacters(
             .fillMaxSize()
             .paint(
                 painter = env.backgroundImage,
-                contentScale = ContentScale.FillBounds
+                contentScale = ContentScale.FillBounds,
+//                colorFilter = ColorFilter.tint(characterColor),
             ),
     )
     {
@@ -245,28 +249,31 @@ fun ScreenCharacters(
 
         LazyRow(
             modifier = modifier
-                .padding(start = 24.dp, bottom = 48.dp, end = 24.dp, top = 48.dp),
-            horizontalArrangement = Arrangement.spacedBy(24.dp),
+                .padding(bottom = 48.dp, top = 48.dp),
+//            horizontalArrangement = Arrangement.spacedBy(24.dp),
             state = lazyListState,
             flingBehavior = snapFlip,
         ) {
             items(env.characters.size) { index ->
+                characterColor = Color.Black.copy(alpha = .2f*index)
                 Box(
                     modifier = modifier
+                        .padding(start = 16.dp, end = 16.dp)
+                        .clip(shape = RoundedCornerShape(25.dp))
                         .aspectRatio(360f / 800f)
-//                        .fillMaxHeight()
-                        .border(4.dp, Color.Black)
-//                        .wrapContentWidth(Alignment.CenterHorizontally)
                         .background(Color.White)
-                        .clickable(onClick = { navController.navigate("/characters/$index") })
+                        .clickable(onClick = { navController.navigate("/characters/$index") }),
                 ) {
                     val character = env.characters[index]
 
                     Image(
                         painter = character.painter,
                         contentDescription = character.name,
-                        modifier = modifier.matchParentSize()
+                        modifier = modifier.matchParentSize(),
+
+                        contentScale = ContentScale.Crop,
                     )
+
                     Text(
                         buildAnnotatedString {
                             withStyle(style = ParagraphStyle(lineHeight = 32.sp)) {
@@ -274,14 +281,17 @@ fun ScreenCharacters(
                             }
                         },
                         modifier = textModifier
-                            .fillMaxSize()
+                            .fillMaxHeight()
                             .padding(
                                 bottom = 20.dp,
                                 start = 12.dp,
-                                end = 24.dp,
+                                end = 12.dp,
                             )
-                            .wrapContentHeight(Alignment.Bottom),
-                        textAlign = TextAlign.Left,
+                            .wrapContentHeight(Alignment.Bottom)
+                            .clip(shape = RoundedCornerShape(25.dp))
+                            .background(Color.White.copy(alpha = 1f)),
+                        color = Color.Black,
+                        textAlign = TextAlign.Center,
                         fontWeight = FontWeight.Bold,
                         fontSize = 32.sp,
                     )
@@ -307,7 +317,10 @@ fun ScreenCharacterInformationNoScuffs(env: Env, navController: NavHostControlle
     ) {
         IconButton(
             onClick = { navController.navigateUp() },
-            modifier = Modifier.padding(top=12.dp)
+            modifier = Modifier
+                .padding(top=12.dp)
+                .clip(shape = CircleShape)
+                .background(color = Color.White.copy(alpha = .4f))
         ) {
             Icon(
                 imageVector = Icons.AutoMirrored.Filled.ArrowBack,
@@ -317,6 +330,7 @@ fun ScreenCharacterInformationNoScuffs(env: Env, navController: NavHostControlle
         Column(
         ) {
             Spacer(Modifier.fillMaxHeight(.4f))
+
             var scrollState = rememberScrollState()
             Text(
                 text =
@@ -333,8 +347,8 @@ fun ScreenCharacterInformationNoScuffs(env: Env, navController: NavHostControlle
                     .fillMaxSize()
                     .wrapContentHeight(Alignment.Bottom)
                     .verticalScroll(state = scrollState)
-                    .background(Color.Black.copy(alpha = .8f)),
-                color = Color.White,
+                    .background(Color.White.copy(alpha = .8f)),
+                color = Color.Black,
             )
         }
     }
